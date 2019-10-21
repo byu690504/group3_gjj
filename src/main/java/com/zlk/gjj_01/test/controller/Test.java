8424cfa543b5cd6df69acd62e318452e831d6e4c
kaiguan <byu690504@163.com>
2019-10-21 19:24:37 +0800

    update by thymeleaf

---
+++ src/main/java/com/zlk/gjj_01/test/controller/Test.java
@@ -0,0 +1,17 @@
+package com.zlk.gjj_01.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping(value = "/test")
+public class Test {
+
+    @RequestMapping("/hello")
+    public String helloHtml(Map<String, Object> map) {
+        map.put("hello", "从 Controller 到 Html");
+        return "test";
+    }
+}
