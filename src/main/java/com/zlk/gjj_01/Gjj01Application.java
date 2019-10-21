7f610f0940a2a26544dc39e26a1aa37f9316882c
kaiguan <byu690504@163.com>
2019-10-21 13:58:31 +0800

    group3_gjj

---
+++ src/main/java/com/zlk/gjj_01/Gjj01Application.java
@@ -0,0 +1,13 @@
+package com.zlk.gjj_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gjj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gjj01Application.class, args);
+	}
+
+}
