e2bad2ab4611f2cd6a7c2b8d3df64f8bb6abd453
kaiguan <byu690504@163.com>
2019-10-22 10:27:51 +0800

    agent

---
+++ src/main/java/com/zlk/gjj_01/register/entity/Agent.java
@@ -0,0 +1,35 @@
+package com.zlk.gjj_01.register.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Table
+@Entity
+public class Agent {
+
+    @Id
+    @GeneratedValue(generator = "myGenerator")
+    @GenericGenerator(name = "myGenerator",strategy = "uuid")
+    @Column(name = "agent_id")
+    private String agentId;
+    @Column(name = "agent_name")
+    private String agentName;
+    @Column(name = "agent_password")
+    private String agentPassword;
+    @Column(name = "card_name")
+    private String cardName;
+    @Column(name = "card_number")
+    private String cardNumber;
+    @Column(name = "agent_phone")
+    private String agentPhone;
+    @Column(name = "unit_id")
+    private String unitId;
+    @Column(name = "unit_register_id")
+    private String unitRegisterId;
+
+}
